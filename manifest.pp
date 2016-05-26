ae5be30b477aa5fdacd66850ec2b77ba20041c17
Leszek Grzanka <Leszek.Grzanka@ifj.edu.pl>
2016-05-26 09:23:25 +0200

    810_pre3

---
+++ manifest.pp
@@ -0,0 +1,29 @@
+# An example puppet file which installs CMSSW into 
+# /opt/cms.
+package {["HEP_OSlibs_SL6", "e2fsprogs"]:
+  ensure => present,
+}->
+file {"/etc/sudoers.d/999-cmsbuild-requiretty":
+   content => "Defaults:root !requiretty\n",
+}->
+user {"cmsbuild":
+  ensure => present,
+}->
+file {"/opt":
+  ensure => directory,
+}->
+file {"/opt/cms":
+  ensure => directory,
+  owner => "cmsbuild",
+}->
+package {"cms+cmssw+CMSSW_8_1_0_pre3":
+  ensure             => present,
+  provider           => cmsdist,
+  install_options    => [{
+    "install_prefix" => "/opt/cms",
+    "install_user"   => "cmsbuild",
+    "architecture"   => "slc6_amd64_gcc530",
+    "server"         => "cmsrep.cern.ch",
+    "server_path"    => "cmssw/cms",
+  }]
+}
